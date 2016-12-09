0e1b7f9ee6129f79adb2a0b9a49cb5f2dfaaf5aa
srahman7 <sabiharahman725@gmail.com>
2016-12-08 22:21:17 -0500

    implemented new methods, have yet to add all exceptions

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -24,7 +24,7 @@ public class Barcode implements Comparable<Barcode>{
 	
     }
 
-    private String key(int num){
+    private static String key(int num){
 	//int num = Integer.parseInt(zip.substring(index,index+1));
 	String ans="";
 	if (num==0){
@@ -59,7 +59,47 @@ public class Barcode implements Comparable<Barcode>{
 	}
 	return ans;
     }
-    
+
+    private static int key(String s){
+	int num=0;
+	if (s.equals("||:::")){
+	    num=0;
+	}
+	else if (s.equals(":::||")){
+	    num=1;
+	}
+	else if (s.equals("::|:|")){
+	    num=2;
+	}
+	else if (s.equals("::||:")){
+	    num=3;
+	}
+	else if (s.equals(":|::|")){
+	    num=4;
+	}
+	else if (s.equals(":|:|:")){
+	    num=5;
+	}
+	else if (s.equals(":||::")){
+	    num=6;
+	}
+	else if (s.equals("|:::|")){
+	    num=7;
+	}
+	else if (s.equals("|::|:")){
+	    num=8;
+	}
+	else if (s.equals("|:|::")){
+	    num=9;
+	}
+	else{
+	    throw new IllegalArgumentException("What you entered is not a known number");
+	}
+	return num;
+    }
+	
+	
+	
 
     // postcondition: computes and returns the check sum for _zip
     private int checkSum(){
@@ -84,6 +124,36 @@ public class Barcode implements Comparable<Barcode>{
 	return ans;
     }
 
+    public static String toZip(String s){
+	String ans="";
+	if (s.length()>32 || s.length()<32){
+	    throw new IllegalArgumentException("Incorrect zip length");
+	}
+	if (s.charAt(0)!='|' || s.charAt(31)!='|'){
+	    throw new IllegalArgumentException("The barcode you entered did not begin or end with a |");
+	}
+	for (int x=1;x<26;x+=5){
+	    ans+=key(s.substring(x,x+5));
+	}
+	return ans;
+    }
+
+    public static String toCode(String z){
+	String ans="|";
+	int check=0;
+	for (int x =0;x<5;x++){
+	    ans+=key(Integer.parseInt(z.substring(x,x+1)));
+	    check+=Integer.parseInt(z.substring(x,x+1));
+	}
+	ans+=key(check%10)+"|";
+	    
+	return ans;
+    }
+	
+	
+	    
+	    
+
 
     // postcondition: compares the zip + checkdigit, in numerical order. 
     public int compareTo(Barcode other){
@@ -98,13 +168,17 @@ public class Barcode implements Comparable<Barcode>{
 	}
     }
     
-    /*public static void main(String[]args){
+    public static void main(String[]args){
 	Barcode a = new Barcode("08451");
 	Barcode b= new Barcode("12345");
+	Barcode c = new Barcode("90210");
 	System.out.println(a.toString());
-	System.out.println(a.clone(b));
+	//System.out.println(a.clone(b));
 	System.out.println(a.toString());
 	System.out.println(a.compareTo(b));
-	
-	}*/
+	System.out.println(toCode("90210"));
+	System.out.println(c.toString());
+	System.out.println(toZip(toCode("90210")));
+	System.out.println(toZip("|"));
+	}
 }
